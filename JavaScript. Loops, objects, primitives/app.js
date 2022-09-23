b888e6a27ea983a6795ce0a86ab1cbc1262c9c39
Elena <olena.ssizova@gmail.com>
2022-09-23 16:52:01 +0300

    task-8-18

--- JavaScript. Loops, objects, primitives/app.js
+++ JavaScript. Loops, objects, primitives/app.js
@@ -104,3 +104,107 @@
 // console.log(`The coordinates of forth vertex (${vertexX4},${vertexY4})`);
 
 //Task 8
+// let year = +prompt("Enter a year:");
+// if (year % 4 === 0 && !(year % 100 === 0 && year % 400 !== 0)) {
+//   console.log(`The year ${year} is a leap year`);
+// } else {
+//   console.log(`The year ${year} isn't a leap year`);
+// }
+
+//Task 9
+// let number = +prompt("Enter a number:");
+// let description = "";
+
+// if (number === 0) {
+//   description = "нульове ";
+// } else {
+//   description += number > 0 ? "позитивне " : "негативне ";
+//   description += number % 2 ? "непарне " : "парне ";
+// }
+// description += "число";
+// console.log(description);
+
+//Task 10
+// let number = +prompt("Enter a number:");
+// let description = "";
+
+// description += number % 2 ? "непарне " : "парне ";
+// if (number / 100 >= 1) {
+//   description += "тризначне ";
+// } else if (number / 10 >= 1) {
+//   description += "двозначне ";
+// } else {
+//   description += "однозначне ";
+// }
+// description += "число";
+// console.log(description);
+
+//Task 11
+// let i = 0;
+// while (++i < 5) alert(i); //1, 2, 3, 4
+
+// let i = 0;
+// while (i++ < 5) alert(i); //1, 2, 3, 4, 5
+
+//різні - на момент порівняння ++і повертає збільшене значення, а і++ - старе
+
+//Task 12
+// for (let i = 0; i < 5; i++) alert(i); // 0, 1, 2, 3, 4
+//for (let i = 0; i < 5; ++i) alert(i); // 0, 1, 2, 3, 4
+//однакові бо i зростає на 1 в кінці циклу, а не перед alert()
+
+//Task 13
+// for (let i = 2; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//Task 14 
+
+// let i = 0;
+// while (i < 3) {
+//   alert(`number ${i}!`);
+//   i++;
+// }
+
+//Task 15
+// let number = 0;
+// while (number < 100) {
+//   number = prompt("Enter a number above 100:");
+//   if (number === null) break;
+//   number = +number;
+// }
+
+//Task 16
+// let bound = 10;
+// for (let i = 2; i <= bound; i++) {
+//   for (let j = 2; j <= bound; j++) {
+//     if (!(i % j) && i !== j) {
+//       break;
+//     } else if (!(i % j) && i === j) {
+//       console.log(i);
+//       break;
+//     }
+//   }
+// }
+
+//Task 17
+// let average;
+// let number1 = +prompt("Enter a first number:");
+// let number2 = +prompt("Enter a second number:");
+// if (!isNaN(number1) && !isNaN(number2)) {
+//   average = (number1 + number2) / 2;
+//   console.log(average);
+// } else {
+//   console.log("You entered wrong data");
+// }
+
+
+//Task18
+// let number = +prompt("Enter a number:");
+// let square;
+// if (!isNaN(number)) {
+//   square = number ** 2;
+//   console.log(square);
+// }
